6043d9495586c408737de9e62fa79eb57e9250e3
Rozpi <joserozpide@yahoo.es>
2022-07-14 15:54:47 +0000

    cambio140722

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,25 @@
-// import React from "react";
-// import rigoImage from "../../img/rigo-baby.jpg";
-// import "../../styles/home.css";
+import React from "react";
 
-// export const Home = () => (
-// 	<div className="text-center mt-5">
-// 		<h1>Hello Rigo!</h1>
-// 		<p>
-// 			<img src={rigoImage} />
-// 		</p>
-// 		<a href="#" className="btn btn-success">
-// 			If you see this green button, bootstrap is working
-// 		</a>
-// 	</div>
-// );
+import "../../styles/home.css";
+import CardPersonajes from "../cards/CardPersonajes.jsx";
+export const Home = () => {
+    const characters=[{
+        "height": "172",
+        "mass": "77",
+        "hair_color": "blond",
+        "skin_color": "fair",
+        "eye_color": "blue",
+        "birth_year": "19BBY",
+        "gender": "male",
+        "created": "2022-07-12T17:37:15.048Z",
+        "edited": "2022-07-12T17:37:15.048Z",
+        "name": "Luke Skywalker",
+        "homeworld": "https://www.swapi.tech/api/planets/1",
+        "url": "https://www.swapi.tech/api/people/1"
+    }]
+    return(
+	<div className="text-center mt-5">
+		<h1>Hello Rigo!</h1>
+		{characters.map((item)=><CardPersonajes name={item.name}/>)}
+	</div>)
+};
